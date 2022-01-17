55bd5061f5e69101d685a430bae587299741e33b
Arnab Das <arnab9432@gmail.com>
2022-01-17 15:53:27 +0530

    added multiply functionality

---
+++ Multiply.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application
+{
+    public class Multiply
+    {
+        public static void main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            Console.Write("result: {0}", a * b);
+        }
+    }
+}
